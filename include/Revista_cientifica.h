d2309de525479ff70403bd36d9cbfe775f51fd93
MaryoriHilares <mhilaresa@unsa.edu.pe>
2020-06-27 09:23:37 -0500

    clase revista cientifica añadida :)

---
+++ include/Revista_cientifica.h
@@ -0,0 +1,21 @@
+#ifndef REVISTA_CIENTIFICA_H
+#define REVISTA_CIENTIFICA_H
+
+
+class Revista_cientifica
+{
+    string Tit_ar;
+    string Tit_rev;
+    string Autor;
+    string URL;
+    int PagInicial;
+    int PagFinal;
+    public:
+        Revista_cientifica();
+        virtual ~Revista_cientifica();
+        void IngresarDatos();
+        void ImprimirDatos();
+        void EliminarDatos();
+};
+
+#endif // REVISTA_CIENTIFICA_H
